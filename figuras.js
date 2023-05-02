20594c5d4d91fd6171baa5bc14fc152362f2ba70
Gonzalo Anza <gonzaloanza@gmail.com>
2023-05-02 17:38:44 -0400

    codigo con funciones listo

--- figuras.js
+++ figuras.js
@@ -1,19 +1,14 @@
 //Codigo del cuadrado
 
 console.group("Cuadrados");
-//const ladoCuadrado = 5;
-//console.log("Los lados del cuadrado mide: " + ladoCuadrado + "cm");
 
 function perimetroCuadra(lado){
     return lado * 4;
 }  
 
-//console.log("El perimetro del cuadrado es: " + perimetroCuadra + "cm");
-
 function areaCuadrada(lado){
     return lado * lado;
 } 
-//console.log("El area del cuadrado es: " + areaCuadrada + "cm^2");
 
 console.groupEnd();
 
@@ -35,12 +30,10 @@ console.log("La actulra del triangualo es de: "+ alturaTriangulo + "cm");
 function perimetroTriangulo(lado1, lado2, base){
     return lado1 + lado2 + base;
 } 
-//console.log("El perimetro del triangulo es: "+perimetroTriangulo + "cm");
 
 function areaTriangulo(base, altura){
     return (base * altura) / 2;
 } 
-//console.log("El area del triangulo es: "+ areaTriangulo + "cm^2");
 
 console.groupEnd();
 
@@ -52,18 +45,22 @@ const radioCirculo = 4;
 console.log("El radio del circulo es: "+ radioCirculo + "cm");
 
 // Diametro
-const diametroCirculo = radioCirculo * 2;
-console.log("El diametro del circulo es: "+ diametroCirculo + "cm");
+function diametroCirculo(radio){
+    return radio * 2;
+} 
 
 // PI
 const PI = Math.PI;
 console.log("El PI: "+ PI + "cm");
 
 // Circunferencia
-const perimetroCirculo = diametroCirculo * PI;
-console.log("El perimetro del circulo es: "+ perimetroCirculo + "cm");
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+} 
 
 // Area
-const areaCirculo = (radioCirculo * radioCirculo) * PI;
-console.log("El area del circulo es: "+ areaCirculo + "cm^2");
+function areaCirculo(radio){
+    return (radio * radio) * PI;
+} 
 console.groupEnd();
